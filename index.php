4564cd4cd829c8e0436c83808c203d61ed2b074a
Carranya <k.gong82@gmail.com>
2022-09-15 21:40:38 +0200

    builted construct

--- index.php
+++ index.php
@@ -3,10 +3,42 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="karin.js"></script>
     <title>Karin's Homepage</title>
 </head>
 <body>
-    <div id="main"></div>
+    <div id="main">
+        <input type='checkbox' id='topleft-check' class='topleft-check'>
+        <div class='home'>
+            <label class='topleft-label' for='topleft-check'></label>
+            <?php
+                include "pages/home.php";
+            ?>
+        </div>
+
+        <input type='checkbox' id='topright-check' class='topright-check'>
+        <div class='home'>
+            <label class='topright-label' for='topright-check'></label>
+            <?php
+                include "pages/projects.php";
+            ?>
+        </div>
+
+        <input type='checkbox' id='bottomleft-check' class='bottomleft-check'>
+        <div class='home'>
+            <label class='bottomleft-label' for='bottomleft-check'></label>
+            <?php
+                include "pages/about.php";
+            ?>
+        </div>
+
+        <input type='checkbox' id='bottomright-check' class='bottomright-check'>
+        <div class='home'>
+            <label class='bottomright-label' for='bottomright-check'></label>
+            <?php
+                include "pages/contact.php";
+            ?>
+        </div>
+
+    </div>
 </body>
 </html>
--- karin.js
+++
@@ -1,5 +0,0 @@
-let head = document.createElement("div");
-head.id = "head";
-document.getElementById("main").append(head);
-
-document.getElementById("head").innerText = "Karin's Homepage";
