51d433fd4295ad568a97618a75e3f5d10f331af6
Carranya <k.gong82@gmail.com>
2022-09-15 14:54:58 +0200

    tested javascript

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="karin.js"></script>
     <title>Karin's Homepage</title>
 </head>
 <body>
-
+    <div id="main"></div>
 </body>
 </html>
---
+++ karin.js
@@ -0,0 +1,5 @@
+let head = document.createElement("div");
+head.id = "head";
+document.getElementById("main").append(head);
+
+document.getElementById("head").innerText = "Karin's Homepage";
