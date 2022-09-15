fdd38202587f9397c86e080272edc2d5f5643ed0
Carranya <k.gong82@gmail.com>
2022-09-15 22:48:27 +0200

    created menu-cards

--- index.php
+++ index.php
@@ -4,38 +4,44 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Karin's Homepage</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="main">
-        <input type='checkbox' id='topleft-check' class='topleft-check'>
-        <div class='home'>
-            <label class='topleft-label' for='topleft-check'></label>
+    <div class="main">
+        <input type='radio' name='menu' id='topleft-check' class='topleft-check'>
+        <div class='topleft' id='content'>
+            <label class='topleft-label' for='topleft-check' id='check-label'></label>
+            <h1>Home</h1>
             <?php
-                include "pages/home.php";
+                
+                //include "pages/home.php";
             ?>
         </div>
 
-        <input type='checkbox' id='topright-check' class='topright-check'>
-        <div class='home'>
-            <label class='topright-label' for='topright-check'></label>
+        <input type='radio' name='menu' id='topright-check' class='topright-check'>
+        <div class='topright'id='content'>
+            <label class='topright-label' for='topright-check' id='check-label'></label>
+            <h1>Projects</h1>
             <?php
-                include "pages/projects.php";
+                //include "pages/projects.php";
             ?>
         </div>
 
-        <input type='checkbox' id='bottomleft-check' class='bottomleft-check'>
-        <div class='home'>
-            <label class='bottomleft-label' for='bottomleft-check'></label>
+        <input type='radio' name='menu' id='bottomleft-check' class='bottomleft-check'>
+        <div class='bottomleft'id='content'>
+            <label class='bottomleft-label' for='bottomleft-check' id='check-label'></label>
+            <h1>About</h1>
             <?php
-                include "pages/about.php";
+                //include "pages/about.php";
             ?>
         </div>
 
-        <input type='checkbox' id='bottomright-check' class='bottomright-check'>
-        <div class='home'>
-            <label class='bottomright-label' for='bottomright-check'></label>
+        <input type='radio' name='menu' id='bottomright-check' class='bottomright-check'>
+        <div class='bottomright'id='content'>
+            <label class='bottomright-label' for='bottomright-check' id='check-label'></label>
+            <h1>Contact</h1>
             <?php
-                include "pages/contact.php";
+                //include "pages/contact.php";
             ?>
         </div>
 
