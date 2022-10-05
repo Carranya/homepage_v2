db707b06214c2bb2baf62c97bb961f35d6fe3b8a
Carranya <k.gong82@gmail.com>
2022-10-05 09:03:22 +0200

    added page bombmemory

---
+++ pages/projects/bombmemory.php
@@ -0,0 +1,10 @@
+<div style="border:1px solid black; padding:10px; margin: 10px">
+<center><a href=''><b><u>Bomb Memory</u></b></a><br></center>
+<br>
+<center><img src="img/screenshots/bombmemory.jpg"></img></center>
+<p>Das klassische Memory Spiel mit neuen Spielerlebnis.<br>
+Versuche mit möglichst wenigen Versuchen alle Paaren zu finden.<br>
+Aber Vorsicht! Wenn du aber die Bombe erwischt -> Game Over.<br>
+(Speziell für Transformers G1 Fans habe ich für die 1. Version Transformers Bilder eingebaut.)
+</p>
+</div>
