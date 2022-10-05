e0ef510035cb764182cd60069f04c5f768511844
Carranya <k.gong82@gmail.com>
2022-10-05 09:03:29 +0200

    added page codecracker

--- pages/projects/codecracker.php
+++ pages/projects/codecracker.php
@@ -1,9 +1,8 @@
 <div class="projectsContent">
-<a href=""><b><u>Code Cracker</u></b></a><br>
-<br>
-<a href=""><img src="img/screenshots/codecracker.jpg"></img></a>
-<p>Ein Mastermind-Like Browser Game für zwischendurch.<br>
-Schaffst du den Code zu knacken?<br>
-<i>Aktuelle Version: 1.0</i>
-</p>
+    <a href=""><b><u>Code Cracker</u></b></a><br>
+    <br>
+    <a href=""><img src="img/screenshots/codecracker.jpg"></img></a>
+    <p>Ein lustiges, Mastermind-Like Browser Game für zwischendurch.<br>
+    Schaffst du nach 10 Versuche den zufälligen, geheimen Code zu knacken?
+    </p>
 </div>
