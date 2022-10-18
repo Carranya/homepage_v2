9f3c902b0849e6e290a3563753120e80cdd34854
Carranya <k.gong82@gmail.com>
2022-10-19 00:05:15 +0200

    added project slidepuzzle

--- pages/projects/slidepuzzle.php
+++ pages/projects/slidepuzzle.php
@@ -1,10 +1,14 @@
 <div class="projectsContent">
-<a href=""><b><u>Slide Puzzle</u></b></a><br>
-<br>
-<a href=""><img src="img/screenshots/slidepuzzle.jpg"></img></a>
-<p>Ein Browserspiel gegen Langeweile<br>
-Versuchen Sie mit möglichst wenig Züge das Bild zusammenzusetzen.<br>
-Sie können auch Ihr Lieblingsfoto für das Spiel hochladen.<br>
-<i>Aktuelle Version: 1.2</i>
-</p>
+    <form action="" target="_blank">
+        <button class="projectsButton">
+            <h2>Slide Puzzle</h2>
+            
+            <img src="img/screenshots/slidepuzzle.jpg"></img>
+
+            <p>Ein Browserspiel gegen Langeweile<br>
+            Versuchen Sie mit möglichst wenig Züge das Bild zusammenzusetzen.<br>
+            Sie können auch Ihr Lieblingsfoto für das Spiel hochladen.  
+            </p>
+        </button>
+    </form>
 </div>
