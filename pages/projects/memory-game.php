a30c34891ee7add2e559d74111061caff67eada2
Carranya <k.gong82@gmail.com>
2022-11-02 14:45:59 +0100

    added new projects

---
+++ pages/projects/memory-game.php
@@ -0,0 +1,15 @@
+<div class="projectsContent">
+    <form action="/projects/memory-game/" target="_blank">
+        <button class="projectsButton">
+            <h2>Memory Game</h2>
+            
+            <img src="img/screenshots/memory-game.jpg"></img>
+
+            <p>Das klassische Memory Game für Zwischendurch.<br>
+            Es hat mehrere, wählbare Themen.<br>
+            Weitere Themen werden bei späteren Updates hinzugefügt.<br>
+            Viele Runden brauchst du, um alle Paare zu finden?<br>
+            </p>
+        </button>
+    </form>
+</div>
---
+++ pages/projects/treasure-digger.php
@@ -0,0 +1,14 @@
+<div class="projectsContent">
+    <form action="/projects/treasure-digger/" target="_blank">
+        <button class="projectsButton">
+            <h2>Treasure Digger</h2>
+            
+            <img src="img/screenshots/treasure-digger.jpg"></img>
+
+            <p>Das Browsergame erinnert man an das klassische Schiffe-versenken Spiel.<br>
+            Suche den Schatz. Sobald du eine Münze findest, ist die Truhe nicht mehr weit.<br>
+            Wieviele Runden brauchst du, um alle Schätze zu finden?<br>
+            </p>
+        </button>
+    </form>
+</div>
