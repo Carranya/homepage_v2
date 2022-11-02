307af10454b35b7bc2fbea960aa02b580778cb8c
Carranya <k.gong82@gmail.com>
2022-11-02 14:46:19 +0100

    ready to upload

--- pages/projects.php
+++ pages/projects.php
@@ -1,24 +1,26 @@
-<h2>Meine Projekte</h2>
-
-<p>In meiner Freizeit programmiere ich sehr gerne.<br>
-Schon seit ich ein Kind war, interessiere ich mich für das Programmieren.<br>
-Bisher habe ich PERL, PHP, MySQL, HTML, CSS und Javascript angeeignet.
-</p>
-
-<h3>GitHub</h3>
 <div id="projectsGitHub">
-<a href='https://github.com/Carranya'><img src="img/icons/github.png" id="projectsGitHubImg"></a>
+    <h2>Meine Projekte</h2>
     
-<p>Alle Codes von meinen Projekten sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
+    <p>In meiner Freizeit programmiere ich sehr gerne.<br>
+    Schon seit ich ein Kind war, interessiere ich mich für das Programmieren.<br>
+    Bisher habe ich PERL, PHP, MySQL, HTML, CSS und Javascript angeeignet.
+    </p>
+    
+    <h3>GitHub</h3>
+    <a href='https://github.com/Carranya'><img src="img/icons/github.png" id="projectsGitHubImg"></a>
+        
+    <p>Alle Codes von meinen Projekten sind in meiner <b><a href='https://github.com/Carranya'>GitHub Seite</a></b> einsehbar.</p>
 </div>
 
 <h3>Meine aktuellen Projekte:</h3>
 <div id="projectsInline">
     <?php
         include "pages/projects/bombmemory.php";
-        include "pages/projects/voki.php";
+        // include "pages/projects/voki.php";
         include "pages/projects/kopfrechnen.php";
-        include "pages/projects/slidepuzzle.php";
+        // include "pages/projects/slidepuzzle.php";
         include "pages/projects/codecracker.php";
+        include "pages/projects/memory-game.php";
+        include "pages/projects/treasure-digger.php";
     ?>
 </div>
