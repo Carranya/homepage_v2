5157db14a0186e7246f170d0f7dd43c285b71e4e
Carranya <k.gong82@gmail.com>
2022-11-06 10:15:21 +0100

    centered pics

--- pages/websites.php
+++ pages/websites.php
@@ -1,11 +1,11 @@
 <h1>Webseiten Vorschläge</h1>
 
 <p>
-Am Liebsten programmiere ich Webseiten.<br>
+Am Liebsten programmiere ich Webseiten und Webtools.<br>
 Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
 <br>
 <p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya"><img src="img/icons/github.png" class="contactBigIcons"></img></a>-Seite</b> einsehbar.</p>
-
+<div class="centerPage">
 <div id="websitesContents">
     <p>
         <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></img></a>
@@ -13,4 +13,5 @@ Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und
         <a href="projects/website_proposal_2/index.php" target="_blank"><img src="img/websites/website_proposal_2.jpg" class="websitesPic"></img></a>
         <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></img></a>
     </p>
+</div>
 </div>
