b7d711f2b167251d16b361068264ae8c246e517a
Carranya <k.gong82@gmail.com>
2022-11-06 10:15:31 +0100

    profile Page done

--- pages/profile.php
+++ pages/profile.php
@@ -4,25 +4,31 @@
 }
 
 
+#test {
+    justify-content: center;
+}
+
 </style>
 
 
 <h1>Profil</h1>
-<p>Ich bin ständig am neue Technologien zu erlernen. </p>
+<p>
+    Ich war 23 Jahre lang als Metallbaukonstrukteur tätig. Aber aus gesundheitlichen Gründen musste ich meinen erlernten Beruf aufgeben. Da ich mich seit meiner Kindheit bereits an Computertechnik interessiere, habe ich entschieden, in die IT-Branche einzusteigen.
+    Unter der verschiedenen Zweigen in der IT-Branche interessiere ich mich sehr für die Webentwicklung. Seither erstelle ich Webtools und Webseiten und lerne ständig neue Programmiersprachen und Technologien.
+</p>
 <br>
-<div class="centerPage">
+<div id="profileLayout">
     <div>
         <h2>Programmiersprachen</h2>
         <div class="profileInline">
             <?php
                 $codesLang = array("HTML5", "CSS3", "PHP8.1", "Javascript", "MySQL", "Tailwind CSS", "Twig");
-                
                 for($i=0; $i<count($codesLang); $i++){
                     echo "<div class='profileCodes'>";
                     echo "<p>" . $codesLang[$i]. "</p>";
                     echo "</div>";
                 }
-                ?>
+            ?>
         </div>
 
 
@@ -36,13 +42,17 @@
                     echo "<p>" . $usePrograms[$i]. "</p>";
                     echo "</div>";
                 }
-                ?>
+            ?>
         </div>
     </div>
 
     <div class="centerPic">
         <br><br><br>
         <img src="img/karin.jpg" alt="Karin Giang" class="showPic"></img>
+        <br><br>
+        <div id="cv">
+            <a href="" target="_blank">Lebenslauf herunterladen (PDF)</a>
+        </div>
     </div>
 </div>
 
