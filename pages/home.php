fd8b44524c1260c4f0d6dd89275da8d55a7f4937
Carranya <k.gong82@gmail.com>
2022-11-07 16:23:13 +0100

    mod.text

--- pages/home.php
+++ pages/home.php
@@ -19,7 +19,7 @@ Bisher habe ich bereits kleine Webtools sowie auch Webpages programmiert.
 <br><br>
 In offiziellen Dokumenten verwende ich noch den männlichen Namen Martin.
 Jedoch in der Freizeit werden meine Freunde mich immer mehr als weiblich ansprechen und ansehen.
-Momentan bin ich noch sehr beschäftigt mit der beruflichen Wandel. Sobald ich mit der neuen Branche stabilisiert habe, werde ich mit der Hormontheraphie beginnen.</p>
+Momentan bin ich noch sehr beschäftigt mit der beruflichen Wandel. Sobald ich mit der neuen Beruf stabilisiert habe, werde ich mit der Hormontheraphie beginnen.</p>
 
 <div id="homeFooter">
     <?php include "pages/footer.php"; ?>
