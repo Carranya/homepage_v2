529e47164922b067b6f75fd0bba08b165bff8f58
Carranya <k.gong82@gmail.com>
2022-11-12 12:45:33 +0100

    mod. websites

--- pages/websites.php
+++ pages/websites.php
@@ -2,8 +2,8 @@
 
 <p>
 Am Liebsten programmiere ich Webseiten und Webtools.<br>
-Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
-<p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" id="websitesLogo"></img></a>-Seite</b> einsehbar.</p>
+Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.<br>
+Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" id="websitesLogo"></img></a>-Seite</b> einsehbar.</p>
 <div class="centerPage">
 <div id="websitesContents">
     <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></a>
