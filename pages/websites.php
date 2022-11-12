98f6c90cbffd2f2df8b0137be7a2cc4070c19644
Carranya <k.gong82@gmail.com>
2022-11-12 12:31:05 +0100

    mod. websites

--- pages/websites.php
+++ pages/websites.php
@@ -3,15 +3,12 @@
 <p>
 Am Liebsten programmiere ich Webseiten und Webtools.<br>
 Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.</p>
-<br>
 <p>Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" id="websitesLogo"></img></a>-Seite</b> einsehbar.</p>
 <div class="centerPage">
 <div id="websitesContents">
-    <p>
-        <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></a>
-        <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></a>
-        <a href="projects/website_proposal_2/index.php" target="_blank"><img src="img/websites/website_proposal_2.jpg" class="websitesPic"></a>
-        <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></a>
-    </p>
+    <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></a>
+    <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></a>
+    <a href="projects/website_proposal_2/index.php" target="_blank"><img src="img/websites/website_proposal_2.jpg" class="websitesPic"></a>
+    <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></a>
 </div>
 </div>
