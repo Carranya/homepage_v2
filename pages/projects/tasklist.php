57f7d75d492e17cdd6fb177f71f484398b72cf1b
Carranya <k.gong82@gmail.com>
2023-01-13 10:32:20 +0100

    update homepage

---
+++ pages/projects/tasklist.php
@@ -0,0 +1,15 @@
+<div class="projectsContent">
+    <form action="/projects/tasklist/index.php" target="_blank">
+        <button class="projectsButton">
+            <h2>Tasklist</h2>
+            
+            <img src="img/screenshots/tasklist.jpg"></img>
+
+            <p>
+            Eine klassische Aufgabenliste für Desktop-PCs, Notebooks und Handys.<br>
+            Einfache Installation. Ganz ohne Datenbanken.<br>
+            (Hinweis: Dieses ist eine Demo-Version. Die Original Version ist in meinem Git-Hub Konto verfügbar.)<br>
+            </p>
+        </button>
+    </form>
+</div>
