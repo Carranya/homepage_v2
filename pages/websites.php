e657a29802938d84202759ae135d729084ba9082
Carranya <k.gong82@gmail.com>
2023-01-13 10:32:18 +0100

    update homepage

--- pages/websites.php
+++ pages/websites.php
@@ -6,6 +6,7 @@ Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und
 Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" id="websitesLogo"></img></a>-Seite</b> einsehbar.</p>
 <div class="centerPage">
     <div id="websitesContents">
+        <a href="projects/vereinhomepagedemo/public_html/index.php" target="_blank"><img src="img/websites/verein.jpg" class="websitesPic"></a>
         <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></a>
         <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></a>
         <a href="projects/website_proposal_2/index.php" target="_blank"><img src="img/websites/website_proposal_2.jpg" class="websitesPic"></a>
